37485994b1de082d9d95b19fb1091af55801a29f
AhmedHKhan18 <ahmed.18hassan.18@gmail.com>
2024-09-11 13:20:35 +0500

    updated

--- src/screens/login.jsx
+++ src/screens/login.jsx
@@ -68,7 +68,7 @@ function handleLogin(e) {
                   <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-primary-800" onClick={handleLogin}>Login</button>
 }
                   <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                      Don`t have an account? <a href="./signup" className="font-medium text-primary-600 hover:underline dark:text-primary-500">SignUp here</a>
+                      Don`t have an account? <a href="./signup" className="font-medium text-primary-600 hover:underline dark:text-primary-500">SignUphere</a>
                   </p>
               </form>
           </div>
--- src/screens/signup.jsx
+++ src/screens/signup.jsx
@@ -81,7 +81,7 @@ export default function SignUp() {
                         <button type="submit" className="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800" onClick={handleSignup}>Create an account</button>
 }
                         <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                            Already have an account? <a href="/login" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Login here</a>
+                            Already have an account? <a href="/login" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Loginhere</a>
                         </p>
                     </form>
                 </div>
